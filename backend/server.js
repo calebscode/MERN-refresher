d442d243a23db6801968355f9f7585c320165f51
calebscode <calebomalley@ufl.edu>
2023-10-25 18:00:58 -0400

    Added api routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // create express app
 const app = express()
 const PORT = process.env.PORT
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // route handler
-app.get('/', (req, res) => {
-    res.json({msg: 'welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
 // listen for requests
 app.listen(PORT, () => {
