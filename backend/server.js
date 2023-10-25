bb5946f5f20fb342f42ee65c1b2e9d9d6e8e4e2d
calebscode <calebomalley@ufl.edu>
2023-10-25 19:03:36 -0400

    added mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // create express app
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 // route handler
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(PORT, () => {
+            console.log(`connected to mongo... listening on port ${PORT}...`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
