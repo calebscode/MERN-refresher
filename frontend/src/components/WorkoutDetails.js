5c05c989c15787add950d0a211d1189fa6a5d2de
Caleb O'Malley <calebomalley@ufl.edu>
2023-10-27 16:36:24 -0400

    added and styled workout details

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,14 @@
+
+const WorkoutDetails = ({ workout }) => {
+    return (
+        <div className="workout-details">
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg): {workout.load}</strong></p>
+            <p><strong>Reps: {workout.reps}</strong></p>
+            <p>{workout.createdAt}</p>
+            
+        </div>
+    )
+}
+
+export default WorkoutDetails
