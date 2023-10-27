798c000c2d165d7c7e48d6a32b6ad97dbf899c06
Caleb O'Malley <calebomalley@ufl.edu>
2023-10-27 16:06:55 -0400

    added basic styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages and components
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <div className="pages">
             <Routes>
-                <Route>
-                    path = '/'
-                    element = {<Home />}
-                </Route>
+                <Route
+                  path = "/"
+                  element = {<Home />}
+                />
             </Routes>
         </div>
       </BrowserRouter>
